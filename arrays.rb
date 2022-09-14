c57d09b4409c497a0497d665e5e7d9789528dbfc
Meredith Trevino <meredithtrevino@gmail.com>
2022-09-14 08:55:45 -0600

    add arrays

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+friends = ['jess', 'joe', 'nari', 'julia' ]
+
+number_of_pets = ['0', '1', '2', '3', '4']
+
+cost_of_lunch = ['8.00', '10.50', '14.50', '0.00',]
+
+has_paid = ['true', 'false', 'true', 'false']
