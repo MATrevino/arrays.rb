30157fd9b4c09ac889a1a50078303bcdee470322
Meredith Trevino <meredithtrevino@gmail.com>
2022-09-14 09:04:10 -0600

    add methods

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,19 @@ number_of_pets = ['0', '1', '2', '3', '4']
 cost_of_lunch = ['8.00', '10.50', '14.50', '0.00',]
 
 has_paid = ['true', 'false', 'true', 'false']
+
+#The code below calls the pop method on the friends array. This will remove the last element
+
+friends.pop
+
+#The code below calls the push method on the number_of_pets array. This will push will add an element at the end of the array.
+
+number_of_pets.push
+
+#Ths code below calls the shift method on the cost_of_lunch array. This will remove the first element.
+
+cost_of_lunch.shift
+
+#This code below calls the unshift method on the has_paid array. This will adds an element to the start of the array.
+
+has_paid.unshift
